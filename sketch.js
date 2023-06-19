89af292976ba4433d17a4b71c57592f0a890661f
kaitabuchi <93952418+kaitabuchi314@users.noreply.github.com>
2023-06-19 10:22:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
+let b;
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  b = new Sphere(width/2, height/2, 0);
 }
 
 function draw() {
   background(51);
-  translate(width/2, height/2, 0);
-  specularColor(255, 255, 255);
-  sphere(40);
+  b.pos.x = width/2;
+  b.pos.y = height/2
+  b.show();
 }
 
 function windowResized() {
